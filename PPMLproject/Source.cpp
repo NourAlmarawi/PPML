5fe5eb4f6ca51a5f86f736310fe2db3b5ead00fe
NourAlmarawi <noorhalmarawe@gmail.com>
2025-05-19 01:42:51 +0300

    secure sum function

--- PPMLproject/Source.cpp
+++ PPMLproject/Source.cpp
@@ -12,6 +12,20 @@ namespace fs = std::experimental;
 using std::vector;
 using namespace std;
 
+vector<int64_t> secureSum(vector<int64_t> &TVGlobal)
+{
+    int m = TVGlobal.size();
+    vector<int64_t> temp1 = TVGlobal;
+    vector<int64_t> temp2 = TVGlobal;
+    for (int i = 0; i < (m / 2) - 1; ++i)
+    {
+        temp1 = temp2;
+        // TODO: replace rotation and addition with homomorphic rotation and addition
+        rotate(temp2.begin(), temp2.begin() + pow(2, i), temp2.end());
+        for (size_t idx = 0; idx < temp2.size(); ++idx) temp2[idx] += temp1[idx];
+    }
+    return temp2;
+}
 
 int main()
 {
